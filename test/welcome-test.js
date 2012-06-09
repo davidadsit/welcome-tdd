db1a375754c6307879bb0ca2ed15800a4e07e333
David Adsit <david.adsit@gmail.com>
2012-06-09 12:02:06 -0600

    spike code

--- test/welcome-test.js
+++ test/welcome-test.js
@@ -1,21 +1,25 @@
 var assert = buster.assert;
 
 buster.testCase("welcome tests", {
+  mockPrinter: null,
 
   setUp: function () {
     buster.assert(true) // buster bug
   },  
 
+  tearDown: function () {
+    welcomePage.readUserName.restore()
+    mockPrinter.restore()
+  },  
+
   "sends welcome message to the printer": function () {
     sinon.stub(welcomePage, "readUserName").returns("David")
-    var mockPrinter = sinon.mock(printer)
+    mockPrinter = sinon.mock(printer)
 
     mockPrinter.expects("printWelcome").withArgs("David").once()
     welcome.greet()
 
     mockPrinter.verify()
-    mockPrinter.restore()
-    welcomePage.readUserName.restore()
   }
 
 });
