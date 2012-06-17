028727bab152e725748749bf57c806f1e41b8a6c
david-adsit <david-adsit@pluralsight.com>
2012-06-16 21:19:41 -0600

    final state of the first kata

--- test/welcome-test.js
+++ test/welcome-test.js
@@ -3,37 +3,38 @@
 buster.testCase("Greeter", {
 
     "uses voice to greet": function() {
-        var voice = this.stub({ say: function(){}, collapse: function(){} });
+        var voice = this.stub();
         var greeter = CreateGreeter(voice);
         greeter.greet();
-        assert.called(voice.say);
-        assert.match(voice.say.firstCall.args[0], /Hello/);
+        assert.called(voice);
+        assert.match(voice.firstCall.args[0], /Hello/);
     }
 });
 
 buster.testCase("Voice", {
-
-//    "say calls console.log": function() {
-//        this.stub(console, "log");
-//        voice.say('sup');
-//        assert.calledWith(console.log, 'sup');
-//    },
-    
-    "appends message to the DOM": function () {
-        var domElement = this.stub({ html: function(){}, show: function(){} });
-
-	var voice = CreateVoice(domElement)
-        voice.say("Hey there")
-
-        assert.calledWith(domElement.html, "Hey there")
+    "speaks to the DOM": function() {
+        this.stub(jQuery.prototype, "html");
+        voice('sup');
+        assert.calledWith(jQuery.prototype.html, 'sup');
     },
-
-    "shows the message": function () {
-        var domElement = this.stub({ html: function(){}, show: function(){} });
-
-	var voice = CreateVoice(domElement)
-        voice.say("This is a test")
-
-        assert.called(domElement.show)
-    }
+    
+    "shows the voicebox": function() {
+        this.stub(jQuery.prototype, "html");
+        this.stub(jQuery.prototype, "show");
+        voice("howdy, y'all");
+        assert.called(jQuery.prototype.show);
+    },
+    
+    "hides the voicebox after 10 seconds": function() {
+        var clock = this.useFakeTimers();
+        this.stub(jQuery.prototype, "html");
+        this.stub(jQuery.prototype, "show");
+        this.stub(jQuery.prototype, "slideUp");
+        
+        voice("howdy, y'all");
+        refute.called(jQuery.prototype.slideUp);
+        clock.tick(10000)
+        assert.called(jQuery.prototype.slideUp);
+        
+   }
 });
